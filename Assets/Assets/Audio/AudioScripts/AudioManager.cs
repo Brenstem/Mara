d9dfec0d8021418b596a4ef7b3fdbdf060f3ac82
Christopher Nylen <Joffynylen@hotmail.com>
2020-04-02 13:57:44 +0200

    Added AudioManager script

---
+++ Assets/Assets/Audio/AudioScripts/AudioManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    private void Awake()
+    {
+
+    }
+    //Player sounds
+    public void PlayerFootStepsAudio()
+    {
+
+    }
+
+    public void PlayerSwordSwingAudio()
+    {
+
+    }
+
+    public void PlayerDodgeAudio()
+    {
+
+    }
+
+    public void PlayerJumpAudio()
+    {
+
+    }
+
+    //Enemy sounds
+
+    public void EnemyStalkerIdleAudio()
+    {
+
+    }
+
+    public void EnemyStalkerDieAudio()
+    {
+
+    }
+    //Music
+
+    public void MenuMusicAudio()
+    {
+
+    }
+
+
+    public void BossMusicAudio()
+    {
+
+    }
+
+    //Stingers
+
+    public void PlayerDeathStingerAudio()
+    {
+
+    }
+
+    public void PlayerRespawnStingerAudio()
+    {
+
+    }
+}
