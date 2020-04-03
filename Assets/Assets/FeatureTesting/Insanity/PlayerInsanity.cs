3646dc1902274a485af0f36de5057043f8770e58
Brenstem <dane.nilsson@gmail.com>
2020-04-03 17:32:26 +0200

    Added basic insanity scripts and enemy script to affect it

---
+++ Assets/Assets/FeatureTesting/Insanity/PlayerInsanity.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInsanity : MonoBehaviour
+{
+    [SerializeField]
+    public float maxInsanity;
+
+    private float currentInsanity;
+
+    void Awake()
+    {
+        // currentInsanity = maxInsanity;
+    }
+
+    private void Update()
+    {
+        print(currentInsanity);
+    }
+
+    // Sets insanity based on parameters
+    public void SetInsanity(float amount)
+    {
+        if (amount > maxInsanity)
+        {
+            currentInsanity = maxInsanity;
+        }
+        else if (amount < 0)
+        {
+            currentInsanity = 0;
+        }
+        else
+        {
+            currentInsanity = amount;
+        }
+    }
+
+    // Increments insanity based on parameters
+    public void IncrementInsanity(float amount)
+    {
+        if (amount + currentInsanity > maxInsanity)
+        {
+            currentInsanity = maxInsanity;
+        }
+        else if (amount + currentInsanity < 0)
+        {
+            currentInsanity = 0;
+        }
+        else
+        {
+            currentInsanity += amount;
+        }
+    }
+}
