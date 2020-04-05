88c13d032ccda10d6b45da4e5d9b3bcaed760a18
Simon <simonalmkvist@gmail.com>
2020-04-05 14:35:58 +0200

    Floating Islands, continued
    Expanded upper section with paths and incentives for players of different skill. This level suggestion requires a bunch of platforming so it might not be suitable for our game, but I wanted to explore it either way. ^^ The lower section could probably use more work.

---
+++ Assets/Assets/Rotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour {
+
+	[SerializeField] GameObject target;
+	[SerializeField] float speed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+		transform.RotateAround(target.transform.position, Vector3.up, speed * Time.deltaTime);
+	}
+}
