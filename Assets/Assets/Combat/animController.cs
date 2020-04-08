17346b5ac05432f89842887fd99781efc1cd5c7c
Dennis Fuchs <backemacke@gmail.com>
2020-04-08 16:56:58 +0200

    Början på de saker jag tappade

---
+++ Assets/Assets/Combat/animController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animController : MonoBehaviour
+{
+
+    Animator anim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+        }
+    }
+
+    public void FaceAndDash()
+    {
+        Vector3 direction = (target.transform.position - this.transform.position);
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookRotation, Time.deltaTime * 100f);
+    }
+}
---
+++ Assets/Assets/Combat/findTargets.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class findTargets : MonoBehaviour
+{
+    [SerializeField] float _radius;
+    LayerMask targets;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Collider[] toHit = Physics.OverlapSphere(transform.position, _radius, targets);
+    }
+
+    private GameObject FindTarget()
+    {
+
+
+        return targetToHit;
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, _radius);
+    }
+}
