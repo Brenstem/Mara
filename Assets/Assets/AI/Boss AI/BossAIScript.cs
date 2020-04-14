82fc0807879dd321a58c2e5b71b643de8f9afede
Alexander Macri <alexander.m.1999@gmail.com>
2020-04-14 17:25:43 +0200

    jobba lite på boss AI

--- Assets/Assets/AI/Boss AI/BossAIScript.cs
+++ Assets/Assets/AI/Boss AI/BossAIScript.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class BossAIScript : MonoBehaviour
 {
@@ -21,6 +20,8 @@ public class BossAIScript : MonoBehaviour
     public PhaseOneStats phaseOneStats;
     public PhaseTwoStats phaseTwoStats;
 
+    public Animator bossAnimator;
+
 
     public StateMachine<BossAIScript> phaseControllingStateMachine;
 
@@ -29,29 +30,40 @@ public class BossAIScript : MonoBehaviour
     public BossPhaseOneState bossPhaseOneState = new BossPhaseOneState();
     public BossPhaseTwoState bossPhaseTwoState = new BossPhaseTwoState();
 
+
+    [SerializeField] public LayerMask targetLayers;
+    
     //galet nog är alla variabler som heter test något, inte planerat att vara permanenta
     [SerializeField] public float testMaxHP = 500f;
     [SerializeField] [Range(0,1)] public float testP2TransitionHP = 0.5f;
 
     [SerializeField] public float testAttackSpeed = 5f;
-    [SerializeField] public float testAttack1DMG = 5f;
-    [SerializeField] public float testAttack1Range = 6f;
-    [SerializeField] public float testAttack1Duration = 7f;
+    [SerializeField] public float testDrainDPS = 5f;
+    [SerializeField] public float testDrainRange = 6f;
+    [SerializeField] public float testDrainChargeTime = 7f;
+    [SerializeField] public float testDrainAttackTime = 8f;
 
     [SerializeField] public float testAttack2DMG = 5f;
 
 
     [SerializeField] public float aggroRange = 10f;
-    [SerializeField] public float turnSpeed = 5f;
-    
-    [SerializeField] public LayerMask targetLayers;
+    [SerializeField] public float defaultTurnSpeed = 5f;
+
+
+    [NonSerialized] public float turnSpeed;
 
     //[SerializeField] public State<BossPhaseOneState>[] stateArray;
 
     //borde vara nonserialized men har den som serialized för testning
     /*[NonSerialized]*/ public float testCurrentHP;
 
+    [NonSerialized] public NavMeshAgent agent;
     [NonSerialized] public GameObject player;
+
+
+    [NonSerialized] public bool animationEnded;
+    [NonSerialized] public bool facePlayer;
+    [NonSerialized] public bool drainHitboxActive;
 
     void Awake()
     {
@@ -61,12 +73,19 @@ public class BossAIScript : MonoBehaviour
         player = GameObject.FindGameObjectWithTag("Player");
 
         testCurrentHP = testMaxHP;
-        testAttack1DMG = phaseOneStats.testP1value1;
+        testDrainDPS = phaseOneStats.testP1value1;
 
+        bossAnimator = GetComponent<Animator>();
+        agent = GetComponentInParent<NavMeshAgent>();
+
+        turnSpeed = defaultTurnSpeed;
+
+        
     }
 
     void Start()
     {
+        agent.updateRotation = false;
         phaseControllingStateMachine.ChangeState(preBossFightState);
     }
     
@@ -92,6 +111,34 @@ public class BossAIScript : MonoBehaviour
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, aggroRange);
     }
+
+    //Animation attack events
+    #region Animation attack events
+        //Generella
+    public void TestBossEvent1()
+    {
+        print("hahahhahahha 1");
+    }
+
+    public void EndAnimation()
+    {
+        animationEnded = true;
+    }
+
+    public void DontFacePlayer()
+    {
+        facePlayer = false;
+        //print("rotation stopped");
+    }
+        //P1
+    public void FlipDrainHitboxActivation()
+    {
+        drainHitboxActive = !drainHitboxActive;
+    }
+
+    #endregion
+
+
 }
 
 public class PreBossFightState : State<BossAIScript>
@@ -128,26 +175,41 @@ public class PreBossFightState : State<BossAIScript>
 //PHASE 1 STATES//
 //////////////////
 
+    //drain (charge, shoot, stay)
+    //idle/movement/bestämma nästa attack (gå runt lite, vara vänd mot spelaren, bestämma vilket state man ska in i sen, alla states går in i detta state)
+    //slå attack (om nära -> slå, typ?)
+    //dash (dasha, fast vart?)
+
+
+
 #region Phase 1 States
 public class BossPhaseOneState : State<BossAIScript>
 {
+
+    public BossAIScript bossPhaseOneParentScript;
+
     public StateMachine<BossPhaseOneState> phaseOneStateMashine;
 
 
-    public Phase1IdleState phase1IdleState;
+    public PhaseOneCombatState phaseOneCombatState;
     public Phase1Attack1State phase1Attack1State;
 
-    
+    public PhaseOneDrainAttackState phaseOneDrainAttackState;
+
 
     public override void EnterState(BossAIScript owner)
     {
         //fixa detta i konstruktor kanske?
         phaseOneStateMashine = new StateMachine<BossPhaseOneState>(this);
 
-        phase1IdleState = new Phase1IdleState(owner.testAttackSpeed);
-        phase1Attack1State = new Phase1Attack1State(owner.testAttack1DMG, owner.testAttack1Range, owner.testAttack1Duration);
+        phaseOneCombatState = new PhaseOneCombatState(owner.testAttackSpeed, owner.testDrainRange, owner);
+        phase1Attack1State = new Phase1Attack1State(owner.testDrainDPS, owner.testDrainRange, owner.testDrainChargeTime);
 
-        phaseOneStateMashine.ChangeState(phase1IdleState);
+        phaseOneDrainAttackState = new PhaseOneDrainAttackState(owner.testDrainDPS, owner.testDrainRange, owner.testDrainAttackTime, owner.testDrainChargeTime);
+
+        bossPhaseOneParentScript = owner;
+
+        phaseOneStateMashine.ChangeState(phaseOneCombatState);
 
         //spela cool animation :)
     }
@@ -165,47 +227,134 @@ public class BossPhaseOneState : State<BossAIScript>
             owner.phaseControllingStateMachine.ChangeState(owner.bossPhaseTwoState);
         }
 
-        //köra currentstate.uppdate
         phaseOneStateMashine.Update();
 
     }
 }
 
-public class Phase1IdleState : State<BossPhaseOneState>
+public class PhaseOneCombatState : State<BossPhaseOneState>
 {
     private float timer;
     private float _attackSpeed;
+    private float _attackRange;
+    private Vector3 _destination;
 
-    public Phase1IdleState(float attackSpeed)
+    private BossAIScript _ownerParentScript;
+
+    public PhaseOneCombatState(float attackSpeed, float attackRange, BossAIScript ownerParentScript)
     {
         _attackSpeed = attackSpeed;
+        _attackRange = attackRange;
+        _ownerParentScript = ownerParentScript;
     }
 
     public override void EnterState(BossPhaseOneState owner)
     {
-        Debug.Log("in i Phase1IdleState");
+        Debug.Log("in i PhaseOneCombatState");
+        
     }
 
     public override void ExitState(BossPhaseOneState owner)
     {
-        Debug.Log("hej då Phase1IdleState");
+        Debug.Log("hej då PhaseOneCombatState");
+        _ownerParentScript.agent.SetDestination(_ownerParentScript.transform.position);
     }
 
     public override void UpdateState(BossPhaseOneState owner)
     {
 
+        _ownerParentScript.FacePlayer();
+
         timer += Time.deltaTime;
+
+
 
         //kolla om man ska attackera
         if (timer > _attackSpeed)
         {
-            timer = 0;
+            if (_attackRange > Vector3.Distance(_ownerParentScript.transform.position, _ownerParentScript.player.transform.position))
+            {
+                timer = 0;
+                owner.phaseOneStateMashine.ChangeState(owner.phaseOneDrainAttackState);
+            }
+            else
+            {
+                //_destination = _ownerParentScript.player.transform.position + (_ownerParentScript.transform.position - _ownerParentScript.player.transform.position).normalized * (_attackRange - _ownerParentScript.agent.stoppingDistance);
 
-            //bestämma vilken attack (prata med designers för att veta hur (patern, radnom, gameplay baserat (lär bli en massa fula switch cases hursom))) 
-            owner.phaseOneStateMashine.ChangeState(owner.phase1Attack1State);
+                _destination = _ownerParentScript.player.transform.position;
+
+                _ownerParentScript.agent.SetDestination(_destination);
+            }
+        }
+        else
+        {
+            if (timer > _attackSpeed / 2)
+            {
+                _ownerParentScript.agent.SetDestination(_ownerParentScript.transform.position + _ownerParentScript.transform.right * 2);
+            }
+            else
+            {
+                _ownerParentScript.agent.SetDestination(_ownerParentScript.transform.position + _ownerParentScript.transform.right * 2 * -1);
+            }
         }
     }
 }
+
+public class PhaseOneDrainAttackState : State<BossPhaseOneState>
+{
+    private float _damagePerSecond;
+    private float _range;
+    private float _totalDurration;
+    private float _chargeTime;
+    private float timer;
+
+
+    public PhaseOneDrainAttackState(float damagePerSecond, float range, float attackTime, float chargeTime)
+    {
+        _damagePerSecond = damagePerSecond;
+        _range = range;
+        _chargeTime = chargeTime;
+        _totalDurration = chargeTime + attackTime;
+    }
+
+
+    public override void EnterState(BossPhaseOneState owner)
+    {
+        owner.bossPhaseOneParentScript.bossAnimator.SetTrigger("testDrainTrigger");
+        Debug.Log("nu ska jag fan göra PhaseOneDrainAttack >:(, med dessa stats:  damage " + _damagePerSecond + " range " + _range + " totalDurration " + _totalDurration + " chargeTime " + _chargeTime);
+    }
+
+    public override void ExitState(BossPhaseOneState owner)
+    {
+        Debug.Log("hej då PhaseOneDrainAttack");
+        owner.bossPhaseOneParentScript.animationEnded = false;
+        owner.bossPhaseOneParentScript.facePlayer = true;
+    }
+
+    public override void UpdateState(BossPhaseOneState owner)
+    {
+        //kör cool animation som bestämmer när attacken är över istället för durration
+
+        if (owner.bossPhaseOneParentScript.animationEnded)
+        {
+            owner.phaseOneStateMashine.ChangeState(owner.phaseOneCombatState);
+        }
+        else
+        {
+            if (owner.bossPhaseOneParentScript.facePlayer)
+            {
+                owner.bossPhaseOneParentScript.FacePlayer();
+            }
+            if (owner.bossPhaseOneParentScript.drainHitboxActive)
+            {
+                //här ska attacken kunna skada
+                Debug.Log("pew pew pew");
+            }
+        }
+    }
+}
+
+
 
 //kan döpa de bättre när vi vet vad de är typ "P1HeavyAttack"
 public class Phase1Attack1State : State<BossPhaseOneState>
@@ -243,10 +392,11 @@ public class Phase1Attack1State : State<BossPhaseOneState>
         if (timer > _durration)
         {
             timer = 0;
-            owner.phaseOneStateMashine.ChangeState(owner.phase1IdleState);
+            owner.phaseOneStateMashine.ChangeState(owner.phaseOneCombatState);
         }
     }
 }
+
 
 
 #endregion
