f69aacfe67b659d9a43aaeaba0a7319cb142127f
Daniel Windhede <daniel.windhede@gmail.com>
2020-04-16 11:02:50 +0200

    MainMenu

--- Assets/Assets/MainMenu/MenuHandler.cs
+++ Assets/Assets/MainMenu/MenuHandler.cs
@@ -4,15 +4,59 @@ using UnityEngine;
 
 public class MenuHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static MenuHandler _state;
+    public static MenuHandler state
     {
-        
+        get { return _state; }
     }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private GameObject _currentMenu;
+
+    private void Awake()
     {
-        
+        if (_state != null && _state != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _state = this;
+        }
+    }
+
+    private void Start()
+    {
+        if (_currentMenu != null)
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                transform.GetChild(i).gameObject.SetActive(false);
+            }
+            EnableMenu(_currentMenu);
+        }
+    }
+
+    private void EnableMenu(GameObject gameObject)
+    {
+        gameObject.SetActive(true);
+    }
+
+    private void DisableMenu(GameObject gameObject)
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void ChangeMenu(GameObject gameObject)
+    {
+        if (gameObject != _currentMenu)
+        {
+            DisableMenu(_currentMenu);
+            _currentMenu = gameObject;
+            EnableMenu(_currentMenu);
+        }
+        else
+        {
+            Debug.LogWarning("Trying to change to already active menu!", this);
+        }
     }
 }
---
+++ Assets/Assets/MainMenu/MenuSwitch.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSwitch : MonoBehaviour
+{
+    [SerializeField] private GameObject _menuSwitchDestination;
+    public void OnClick()
+    {
+        print("click");
+        MenuHandler.state.ChangeMenu(_menuSwitchDestination);
+    }
+}
