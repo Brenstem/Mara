16c12345f74da31d35153df8e3deb62aace1a4bd
Dennis Fuchs <backemacke@gmail.com>
2020-04-17 16:01:37 +0200

    Idk man

--- Assets/Assets/AI/Basic AI movement/BaseAIMovementController.cs
+++ Assets/Assets/AI/Basic AI movement/BaseAIMovementController.cs
@@ -6,15 +6,9 @@ using UnityEngine.AI;
 
 [RequireComponent(typeof(NavMeshAgent))]
 
-public class BaseAIMovementController : MonoBehaviour
+public abstract class BaseAIMovementController : MonoBehaviour
 {
     public StateMachine<BaseAIMovementController> stateMachine;
-
-    public EnemyBasicIdleState idleState = new EnemyBasicIdleState();
-    public EnemyBasicChasingState chasingState = new EnemyBasicChasingState();
-    public EnemyBasicAttackingState attackingState = new EnemyBasicAttackingState();
-    public EnemyBasicReturnToIdlePosState returnToIdlePosState  = new EnemyBasicReturnToIdlePosState();
-
 
     [SerializeField] public float aggroRange = 10f;
     [SerializeField] public float unaggroRange = 20f;
@@ -25,7 +19,7 @@ public class BaseAIMovementController : MonoBehaviour
 
     [SerializeField] public bool cyclePathing;
     [SerializeField] public Vector3[] idlePathingPoints;
-
+    [SerializeField] public float attackRange;
 
     [NonSerialized] public Vector3 idlePosition;
 
@@ -40,19 +34,18 @@ public class BaseAIMovementController : MonoBehaviour
         agent = GetComponent<NavMeshAgent>();
 
         //borde inte göras såhär at the end of the day men måste göra skit med spelaren då och vet inte om jag får det
-        target = GameObject.FindGameObjectWithTag("Player");
+        target = GlobalState.state.Player;
     }
-
 
     virtual protected void Start()
     {
-        stateMachine.ChangeState(idleState);
+        stateMachine.ChangeState(new BaseIdleState());
     }
-
 
     virtual protected void Update()
     {
         stateMachine.Update();
+        print(stateMachine.currentState);
     }
 
     //vänder monstret mot spelaren
@@ -81,16 +74,13 @@ public class BaseAIMovementController : MonoBehaviour
     }
 }
 
-
 //State classer
-public class EnemyBasicIdleState : State<BaseAIMovementController>
+public class BaseIdleState : State<BaseAIMovementController>
 {
     private RaycastHit _hit;
     private int _pathingIndex = 0;
 
-    public override void EnterState(BaseAIMovementController owner)
-    {
-    }
+    public override void EnterState(BaseAIMovementController owner) { }
 
     public override void ExitState(BaseAIMovementController owner)
     {
@@ -125,76 +115,77 @@ public class EnemyBasicIdleState : State<BaseAIMovementController>
             {
                 if (_hit.transform == owner.target.transform)
                 {
-                    owner.stateMachine.ChangeState(owner.chasingState);
+                    owner.stateMachine.ChangeState(new BaseChasingState());
                 }
             }
         }
     }
 }
 
-public class EnemyBasicChasingState : State<BaseAIMovementController>
+public class BaseChasingState : State<BaseAIMovementController>
 {
-    public override void EnterState(BaseAIMovementController owner)
-    {
-    }
+    public override void EnterState(BaseAIMovementController owner) { }
 
-    public override void ExitState(BaseAIMovementController owner)
-    {
-    }
+    public override void ExitState(BaseAIMovementController owner) { }
 
     public override void UpdateState(BaseAIMovementController owner)
     {
+        float range = owner.agent.stoppingDistance + owner.attackRange;
+
+        //
+        Vector3 vectorToPlayer = (owner.target.transform.position - owner.transform.position).normalized * range;
+        Vector3 targetPosition = owner.target.transform.position - vectorToPlayer; 
+
         //flyttar monstret mot spelaren
-        owner.agent.SetDestination(owner.target.transform.position);
+        owner.agent.SetDestination(targetPosition);
         
         if (owner.unaggroRange < Vector3.Distance(owner.target.transform.position, owner.transform.position))
         {
-            owner.stateMachine.ChangeState(owner.returnToIdlePosState);
+            owner.stateMachine.ChangeState(new BaseReturnToIdlePosState()); ;
         }
 
 
-        if(owner.agent.stoppingDistance > Vector3.Distance(owner.target.transform.position, owner.transform.position))
+        Debug.Log("range " + range);
+        Debug.Log("distance " + Vector3.Distance(owner.target.transform.position, owner.transform.position));
+        if(range > Vector3.Distance(targetPosition, owner.transform.position))
         {
-            owner.stateMachine.ChangeState(owner.attackingState);
+            owner.stateMachine.ChangeState(new BaseAttackingState());
         }
     }
 }
 
 //kommer antagligen få fixa olika states beroende på attacken men de lär se ut ungefär såhär
-public class EnemyBasicAttackingState : State<BaseAIMovementController>
+public class BaseAttackingState : State<BaseAIMovementController>
 {
-    public override void EnterState(BaseAIMovementController owner)
-    {
-    }
+    public override void EnterState(BaseAIMovementController owner) { }
 
-    public override void ExitState(BaseAIMovementController owner)
-    {
-    }
+    public override void ExitState(BaseAIMovementController owner)  { }
 
     public override void UpdateState(BaseAIMovementController owner)
     {
         //lägg in attack metod här
 
+        float range = owner.agent.stoppingDistance + owner.attackRange;
+
+        Vector3 vectorToPlayer = (owner.target.transform.position - owner.transform.position).normalized * range;
+        Vector3 targetPosition = owner.target.transform.position - vectorToPlayer; // target position - (vector between enemy and player with length of range)
+
         owner.FacePlayer();
 
-        if (owner.agent.stoppingDistance < Vector3.Distance(owner.target.transform.position, owner.transform.position))
+        if (range < Vector3.Distance(targetPosition, owner.transform.position))
         {
-            owner.stateMachine.ChangeState(owner.chasingState);
+            owner.stateMachine.ChangeState(new BaseChasingState());
         }
     }
 }
 
-public class EnemyBasicReturnToIdlePosState : State<BaseAIMovementController>
+public class BaseReturnToIdlePosState : State<BaseAIMovementController>
 {
     private RaycastHit _hit;
 
-    public override void EnterState(BaseAIMovementController owner)
-    {
-    }
+    public override void EnterState(BaseAIMovementController owner) { }
 
-    public override void ExitState(BaseAIMovementController owner)
-    {
-    }
+    public override void ExitState(BaseAIMovementController owner) { }
 
     public override void UpdateState(BaseAIMovementController owner)
     {
@@ -208,14 +199,14 @@ public class EnemyBasicReturnToIdlePosState : State<BaseAIMovementController>
             {
                 if (_hit.transform == owner.target.transform)
                 {
-                    owner.stateMachine.ChangeState(owner.chasingState);
+                    owner.stateMachine.ChangeState(new BaseChasingState());
                 }
             }
         }
 
         if (owner.agent.stoppingDistance > Vector3.Distance(owner.transform.position, owner.idlePosition))
         {
-            owner.stateMachine.ChangeState(owner.idleState);
+            owner.stateMachine.ChangeState(new BaseIdleState());
         }
 
     }
---
+++ Assets/Assets/AI/Basic Enemy AI/Scripts/DWEnemyAI.cs
@@ -0,0 +1,166 @@
+﻿
+using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+
+public class DWEnemyAI : MonoBehaviour
+{
+    public StateMachine<DWEnemyAI> stateMachine;
+
+    [SerializeField] public float aggroRange = 10f;
+    [SerializeField] public float unaggroRange = 20f;
+    [SerializeField] public float turnSpeed = 5f;
+
+    //Layermask skit för line of sight raycasts
+    [SerializeField] public LayerMask targetLayers;
+
+    [SerializeField] public bool cyclePathing;
+    [SerializeField] public Vector3[] idlePathingPoints;
+
+
+    [NonSerialized] public Vector3 idlePosition;
+
+    /*[NonSerialized] */
+    public GameObject target;
+    [NonSerialized] public NavMeshAgent agent;
+
+    virtual protected void Awake()
+    {
+        idlePosition = this.transform.position;
+        stateMachine = new StateMachine<DWEnemyAI>(this);
+
+        agent = GetComponentInParent<NavMeshAgent>();
+
+        //target = GameObject.FindGameObjectWithTag("Player"); // Byt till globalstate.state.player
+    }
+
+
+    virtual protected void Start()
+    {
+        stateMachine.ChangeState(new ShadowIdleState());
+    }
+
+
+    virtual protected void Update()
+    {
+        stateMachine.Update();
+    }
+
+    //vänder monstret mot spelaren
+    virtual public void FacePlayer()
+    {
+        Vector3 direction = (target.transform.position - this.transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookRotation, Time.deltaTime * turnSpeed);
+    }
+
+    virtual protected void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, aggroRange);
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, unaggroRange);
+
+        Gizmos.color = Color.blue;
+        if (idlePathingPoints.Length > 1)
+        {
+            for (int i = 0; i < idlePathingPoints.Length - 1; i++)
+            {
+                Gizmos.DrawLine(idlePathingPoints[i], idlePathingPoints[i + 1]);
+            }
+        }
+    }
+}
+
+public class ShadowIdleState : State<DWEnemyAI>
+{
+    private RaycastHit _hit;
+    public override void EnterState(DWEnemyAI owner)
+    {
+
+    }
+
+    public override void ExitState(DWEnemyAI owner)
+    {
+
+    }
+
+    public override void UpdateState(DWEnemyAI owner)
+    {
+        Debug.Log("idle");
+        //aggro detection
+        if (owner.aggroRange > Vector3.Distance(owner.target.transform.position, owner.transform.position))
+        {
+            Debug.Log("within range");
+            if (Physics.Raycast(owner.transform.position + new Vector3(0, 1, 0), (owner.target.transform.position - owner.transform.position).normalized, out _hit, owner.aggroRange, owner.targetLayers))
+            {
+                Debug.Log("ray hit");
+                Debug.Log(_hit.collider.gameObject);
+                if (_hit.transform == owner.target.transform)
+                {
+                    owner.stateMachine.ChangeState(new ShadowAggroState());
+                }
+            }
+        }
+    }
+}
+
+public class ShadowAggroState : State<DWEnemyAI>
+{
+    private Timer _timer;
+    public override void EnterState(DWEnemyAI owner)
+    {
+        Debug.Log("tru");
+        owner.agent.SetDestination(owner.transform.position);
+        _timer = new Timer(UnityEngine.Random.Range(1.4f, 2.0f));
+    }
+
+    public override void ExitState(DWEnemyAI owner)
+    {
+        _timer.Reset();
+    }
+
+    public override void UpdateState(DWEnemyAI owner)
+    {
+        _timer.Time += Time.deltaTime;
+        if (!_timer.Expired())
+        {
+            //owner.FacePlayer();
+        }
+        else
+        {
+            owner.stateMachine.ChangeState(new ShadowMoveState());
+        }
+    }
+}
+
+
+public class ShadowMoveState : State<DWEnemyAI>
+{
+    private Timer _timer;
+    public override void EnterState(DWEnemyAI owner)
+    {
+        Debug.Log("Moving to target...");
+        _timer = new Timer(0.2f);
+    }
+
+    public override void ExitState(DWEnemyAI owner)
+    {
+        _timer.Reset();
+    }
+
+    public override void UpdateState(DWEnemyAI owner)
+    {
+        _timer.Time += Time.deltaTime;
+        if (!_timer.Expired())
+        {
+            owner.agent.SetDestination(owner.target.transform.position);
+        }
+        else
+        {
+            owner.stateMachine.ChangeState(new ShadowAggroState());
+        }
+    }
+}
